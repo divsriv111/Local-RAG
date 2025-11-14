597fae7befca529449380e759ad89728db919d86
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2025-11-14 16:50:14 -0500

    some fixes

--- rag-chatbot-frontend/src/app/app.config.ts
+++ rag-chatbot-frontend/src/app/app.config.ts
@@ -8,6 +8,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MarkdownModule } from 'ngx-markdown';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
@@ -19,6 +21,14 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '[data-theme="dark"]',
+        },
+      },
+    }),
     importProvidersFrom(MarkdownModule.forRoot()),
   ],
 };
